4abe80e7f492171902dcf8b583b4b035e09a57d2
Patricia Popa <Patricia.Popa@msg.group>
2020-05-26 18:05:49 +0300

    step 102 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/sh
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
+    DropdownDirective,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  constructor() { }
+
+  @HostListener('click') toggleOpen(eventData: Event) {
+    this.isOpen = !this.isOpen;
+  }
+
+}
