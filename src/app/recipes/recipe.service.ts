1783f15211cf6abbe224cefb15d021be12433a7b
Patricia Popa <Patricia.Popa@msg.group>
2020-07-06 16:39:44 +0300

    121. Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,14 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     /* this shoud be encapsulated with a method, but we won't, for simplicity */
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe("A Test Recipe", "This is simply a test", "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg"),
-        new Recipe("A Test Recipe 2", "This is simply a test 2", "")
+        new Recipe("Mom's spaghetti", "A recipe by Eminem", "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg", [new Ingredient('Meat', 1), new Ingredient('Pasta', 2), new Ingredient('Veggies mix', 1)]),
+        new Recipe("Big Burger", "Just delicious", "", [new Ingredient('Meat', 1), new Ingredient('Buns', 2)])
     ];
 
     getRecipes() {
