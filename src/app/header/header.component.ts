6799d039748fa73a8f7e771c1a58c8f0a782ecd9
Patricia Popa <Patricia.Popa@msg.group>
2020-07-08 17:19:02 +0300

    156. Adding navigation to the app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,10 +6,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
     collapsed = true;
-
-    @Output() featureSelected = new EventEmitter<string>();
-
-    onSelect(feature: string) {
-        this.featureSelected.emit(feature);
-    }
 }
