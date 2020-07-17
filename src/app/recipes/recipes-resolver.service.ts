cece8eb8d1b9bb68b3c3c5ea41189622521712d1
Patricia Popa <Patricia.Popa@msg.group>
2020-07-17 11:23:48 +0300

    284. Fixing a bug with the resolver

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeResolverService implements Resolve<Recipe[]> {
 
-    constructor(private dataStorageService: DataStorageService) { }
+    constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) { }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        // preventing the resolver from overriding our recipes, if we already have some
+        const recipes = this.recipeService.getRecipes();
+
+        if (recipes.length > 0) {
+            return recipes;
+        }
+
         return this.dataStorageService.fetchRecipes(); // the Resolver will subscribe for us
     }
 }
