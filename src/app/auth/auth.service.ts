838203d8ffa7d17385bb3f2e82efa1274ffa084d
Patricia Popa <Patricia.Popa@msg.group>
2020-07-17 15:29:09 +0300

    296. Sending login requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean; // the ? indicates this is optional
 }
 
 @Injectable({ providedIn: 'root' })
@@ -35,5 +36,16 @@ export class AuthService {
                 }
                 return throwError(errorMessage);
             }));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDDedlpuTkDBCkaiYtyylYIjFbVJy6zmTw',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        )
     }
 }
