db1806c0ad3a2db607683fce9e4a791d84ca79fc
Patricia Popa <Patricia.Popa@msg.group>
2020-07-20 17:17:25 +0300

    322.*Splitting modules correctly-my code works w/o

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,6 +5,9 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -14,6 +17,14 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
         RecipeItemComponent,
         RecipeStartComponent,
         RecipeEditComponent
+    ],
+    imports: [
+        RouterModule,
+        /* allow us access to *ngIf and *ngFor, just like BrowserModule; however, that one can only
+        be important in one place (in this case, the app.module.ts file), so that here we need to use
+        an altrnative, which is CommonModule*/
+        CommonModule,
+        ReactiveFormsModule 
     ],
     exports: [
         RecipesComponent,
