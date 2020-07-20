34ab78ef11e5ff09f370294b36bc637a5391d8c1
Patricia Popa <Patricia.Popa@msg.group>
2020-07-20 17:11:12 +0300

    321. Getting started with feature modules

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RecipesComponent } from './recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        RecipeEditComponent
+    ],
+    exports: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        RecipeEditComponent
+    ]
+})
+export class RecipesModule { }
