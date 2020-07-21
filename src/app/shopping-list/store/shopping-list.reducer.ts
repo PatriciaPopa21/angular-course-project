bd90375c63f0f8a3faa541da937a4cc5490962b9
Patricia Popa <Patricia.Popa@msg.group>
2020-07-21 16:42:21 +0300

    354. Updating and deleting ingredients

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -15,8 +15,9 @@ export function shoppingListReducer(
     switch (action.type) {
         case ShoppingListActions.ADD_INGREDIENT:
             return {
-                /* here, we use the spread operator to copy the old state, split into its attributes 
-                (the only one we have so far is ingredients, but it's a good practice anyway) */
+                /* here, we use the spread operator to copy the old state, by splitting it into its attributes 
+                (the only one we have so far is ingredients, but it's a good practice anyway); 
+                we do this to make sure we don't override the other fields (in case we had any other) */
                 ...state,
                 ingredients: [...state.ingredients, action.payload]
             };
@@ -24,6 +25,26 @@ export function shoppingListReducer(
             return {
                 ...state,
                 ingredients: [...state.ingredients, ...action.payload]
+            };
+        case ShoppingListActions.UPDATE_INGREDIENT:
+            const ingredient = state.ingredients[action.payload.index];
+            const updatedIngredient = {
+                ...ingredient,
+                ...action.payload.ingredient
+            };
+            const updatedIngredients = [...state.ingredients];
+            updatedIngredients[action.payload.index] = updatedIngredient;
+
+            return {
+                ...state,
+                ingredients: updatedIngredients
+            };
+        case ShoppingListActions.DELETE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: state.ingredients.filter((ig, igIndex) => {
+                    return igIndex !== action.payload;
+                })
             };
         default:
             return state;
