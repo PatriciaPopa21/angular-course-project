40c709cd7254fd1c51f791839a429a944ba14ee9
Patricia Popa <Patricia.Popa@msg.group>
2020-07-21 16:28:25 +0300

    352. Multiple actions

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,6 +3,8 @@ import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Subject } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
 
 @Injectable()
 export class RecipeService {
@@ -15,14 +17,18 @@ export class RecipeService {
 
     private recipes: Recipe[] = [];
 
-    constructor(private shoppingListService: ShoppingListService) { }
+    constructor(
+        private shoppingListService: ShoppingListService,
+        private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+    ) { }
 
     getRecipes() {
         return this.recipes.slice();
     }
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
-        this.shoppingListService.addIngredients(ingredients);
+        // this.shoppingListService.addIngredients(ingredients);
+        this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
     }
 
     getRecipe(id: number) {
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -8,12 +8,22 @@ const initialState = {
     ]
 };
 
-export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
+export function shoppingListReducer(
+    state = initialState,
+    action: ShoppingListActions.ShoppingListActions
+) {
     switch (action.type) {
         case ShoppingListActions.ADD_INGREDIENT:
             return {
-                ...state, // copy the old state
+                /* here, we use the spread operator to copy the old state, split into its attributes 
+                (the only one we have so far is ingredients, but it's a good practice anyway) */
+                ...state,
                 ingredients: [...state.ingredients, action.payload]
+            };
+        case ShoppingListActions.ADD_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: [...state.ingredients, ...action.payload]
             };
         default:
             return state;
