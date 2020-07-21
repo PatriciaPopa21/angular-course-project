8e4ea68b16bbc882dd92b080523e4eea84badd75
Patricia Popa <Patricia.Popa@msg.group>
2020-07-21 18:01:08 +0300

    360. Setting up auth reducer and actions

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
     user: User
@@ -8,6 +9,28 @@ const initialState = {
     user: null
 }
 
-export function authReducer(state = initialState, action) {
-    return state;
+export function authReducer(
+    state = initialState,
+    action: AuthActions.AuthActions
+) {
+    switch (action.type) {
+        case AuthActions.LOGIN:
+            const user = new User(
+                action.payload.email,
+                action.payload.userId,
+                action.payload.token,
+                action.payload.expirationDate
+            );
+            return {
+                ...state,
+                user // shorthand for user: user (because the property and the constant storing the new value have the same name)
+            }
+        case AuthActions.LOGOUT:
+            return {
+                ...state,
+                user: null
+            };
+        default:
+            return state;
+    }
 }
