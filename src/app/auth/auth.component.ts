e1f1ba7d7ae99b138f7117996ebd4dd452b73bc1
Patricia Popa <Patricia.Popa@msg.group>
2020-07-22 12:15:08 +0300

    369. Finishing the login effect

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -31,6 +31,9 @@ export class AuthComponent implements OnInit, OnDestroy {
         this.store.select('auth').subscribe(authState => {
             this.isLoading = authState.loading;
             this.error = authState.authError;
+            if (this.error) {
+                this.showErrorAlert(this.error);
+            }
         });
     }
 
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,10 +1,11 @@
 import { Actions, ofType, Effect } from '@ngrx/effects';
 import * as AuthActions from './auth.actions';
-import { switchMap, catchError, map } from 'rxjs/operators';
+import { switchMap, catchError, map, tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData {
     idToken: string;
@@ -34,22 +35,48 @@ export class AuthEffects {
                 .pipe(
                     map(resData => {
                         const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-                        return of(
-                            new AuthActions.Login({
-                                email: resData.email,
-                                userId: resData.localId,
-                                token: resData.idToken,
-                                expirationDate: expirationDate
-                            }));
+                        return new AuthActions.Login({
+                            email: resData.email,
+                            userId: resData.localId,
+                            token: resData.idToken,
+                            expirationDate: expirationDate
+                        });
                     }),
-                    catchError(error => {
+                    catchError(errorRes => {
                         /* here we must return a non-error observable, so that our stream doesn't die! */
-                        return of();
+                        let errorMessage = 'An unknown error occurred!';
+                        if (!errorRes.error || !errorRes.error.error) {
+                            return of(new AuthActions.LoginFail(errorMessage));
+                        }
+                        switch (errorRes.error.error.message) {
+                            case 'EMAIL_EXISTS':
+                                errorMessage = 'This email already exists';
+                                break;
+                            case 'EMAIL_NOT_FOUND':
+                                errorMessage = 'This email does not exist';
+                                break;
+                            case 'INVALID_PASSWORD': // its best to handle the email and password with the same error, for security reasons; here, we wanted to be more specific, just because we are practicing
+                                errorMessage = 'This password is not correct';
+                                break;
+                        }
+                        return of(new AuthActions.LoginFail(errorMessage));
                     })
                 )
         }),
     );
 
+    @Effect({ dispatch: false }) // this effect will not yield a dispatchable action in the end
+    authSuccess = this.actions$.pipe(
+        ofType(AuthActions.LOGIN),
+        tap(() => {
+            this.router.navigate(['/']);
+        })
+    );
+
     /* actions$ will give us access to ALL the dispatched actions */
-    constructor(private actions$: Actions, private http: HttpClient) { }
+    constructor(
+        private actions$: Actions,
+        private http: HttpClient,
+        private router: Router
+    ) { }
 }
