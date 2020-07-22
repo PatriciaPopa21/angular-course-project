76f75ba00979a5670aeb83b6baa84013e6a8890e
Patricia Popa <Patricia.Popa@msg.group>
2020-07-22 13:30:08 +0300

    371. Adding signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -48,31 +48,16 @@ export class AuthComponent implements OnInit, OnDestroy {
         const email = form.value.email;
         const password = form.value.password;
 
-        let authObs: Observable<AuthResponseData>;
-
-        this.isLoading = true;
         if (this.isLoginMode) {
             // authObs = this.authService.login(email, password);
             this.store.dispatch(
                 new AuthActions.LoginStart({ email: email, password: password })
             );
         } else {
-            authObs = this.authService.signup(email, password);
+            this.store.dispatch(
+                new AuthActions.SignupStart({ email: email, password: password })
+            );
         }
-
-        // authObs.subscribe(
-        //     resData => {
-        //         console.log(resData);
-        //         this.isLoading = false;
-        //         this.router.navigate(['/recipes']);
-        //     },
-        //     errorMessage => {
-        //         console.log(errorMessage);
-        //         this.error = errorMessage;
-        //         this.isLoading = false;
-        //         this.showErrorAlert(errorMessage);
-        //     }
-        // );
 
         form.reset(); // this was actually supposed to be here since a long time :)
     }
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -16,11 +16,69 @@ export interface AuthResponseData {
     registered?: boolean; // the ? indicates this is optional
 }
 
+const handleAuthentication = (
+    expiresIn: number,
+    email: string,
+    userId: string,
+    token: string
+) => {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    return new AuthActions.AuthenticateSuccess({
+        email: email,
+        userId: userId,
+        token: token,
+        expirationDate: expirationDate
+    });
+};
+
+const handleError = (errorRes: any) => {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+        return of(new AuthActions.AuthenticateFail(errorMessage));
+    }
+    switch (errorRes.error.error.message) {
+        case 'EMAIL_EXISTS':
+            errorMessage = 'This email already exists';
+            break;
+        case 'EMAIL_NOT_FOUND':
+            errorMessage = 'This email does not exist';
+            break;
+        case 'INVALID_PASSWORD': // its best to handle the email and password with the same error, for security reasons; here, we wanted to be more specific, just because we are practicing
+            errorMessage = 'This password is not correct';
+            break;
+    }
+    return of(new AuthActions.AuthenticateFail(errorMessage));
+};
+
 @Injectable()
 export class AuthEffects {
     @Effect()
     authSignup = this.actions$.pipe(
-        ofType(AuthActions.SIGNUP_START)
+        ofType(AuthActions.SIGNUP_START),
+        switchMap((signupAction: AuthActions.SignupStart) => {
+            return this.http.post<AuthResponseData>(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
+                {
+                    email: signupAction.payload.email,
+                    password: signupAction.payload.password,
+                    returnSecureToken: true
+                }
+            )
+                .pipe(
+                    map(resData => {
+                        return handleAuthentication(
+                            +resData.expiresIn,
+                            resData.email,
+                            resData.localId,
+                            resData.idToken
+                        );
+                    }),
+                    catchError(errorRes => {
+                        /* here we must return a non-error observable, so that our stream doesn't die! */
+                        return handleError(errorRes);
+                    })
+                )
+        })
     );
 
     /* NgRx will automatically subscribe for us; also, unlike other observables, the actions$ observable is not allowed to die, 
@@ -39,32 +97,16 @@ export class AuthEffects {
                     })
                 .pipe(
                     map(resData => {
-                        const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-                        return new AuthActions.AuthenticateSuccess({
-                            email: resData.email,
-                            userId: resData.localId,
-                            token: resData.idToken,
-                            expirationDate: expirationDate
-                        });
+                        return handleAuthentication(
+                            +resData.expiresIn,
+                            resData.email,
+                            resData.localId,
+                            resData.idToken
+                        );
                     }),
                     catchError(errorRes => {
                         /* here we must return a non-error observable, so that our stream doesn't die! */
-                        let errorMessage = 'An unknown error occurred!';
-                        if (!errorRes.error || !errorRes.error.error) {
-                            return of(new AuthActions.AuthenticateFail(errorMessage));
-                        }
-                        switch (errorRes.error.error.message) {
-                            case 'EMAIL_EXISTS':
-                                errorMessage = 'This email already exists';
-                                break;
-                            case 'EMAIL_NOT_FOUND':
-                                errorMessage = 'This email does not exist';
-                                break;
-                            case 'INVALID_PASSWORD': // its best to handle the email and password with the same error, for security reasons; here, we wanted to be more specific, just because we are practicing
-                                errorMessage = 'This password is not correct';
-                                break;
-                        }
-                        return of(new AuthActions.AuthenticateFail(errorMessage));
+                        return handleError(errorRes);
                     })
                 )
         }),
